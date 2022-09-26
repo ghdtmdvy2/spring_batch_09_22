e511c4d4aafd0cb793205f97e49763d53dc3dc36
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-26 13:53:45 +0900

    장바구니 중복으로 상품 담았을 때 수량 증가

--- src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cart/service/CartService.java
@@ -7,18 +7,30 @@ import com.ll.exam.app_2022_09_22.app.product.entity.ProductOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final CartItemRepository cartItemRepository;
 
     public void addItem(Member member, ProductOption productOption, int quantity) {
-        CartItem cartItem = CartItem.builder()
-                .member(member)
-                .productOption(productOption)
-                .quantity(quantity)
-                .build();
+        Optional<CartItem> cartItem_ = cartItemRepository.findByMember_idAndProductOption_id(member.getId(),productOption.getId());
 
-        cartItemRepository.save(cartItem);
+        if (cartItem_.isPresent()){
+            CartItem cartItem = cartItem_.get();
+            int PrevQuantity = cartItem.getQuantity();
+            PrevQuantity = PrevQuantity + quantity;
+            cartItem.setQuantity(PrevQuantity);
+            cartItemRepository.save(cartItem);
+        } else {
+            CartItem cartItem = CartItem.builder()
+                    .member(member)
+                    .productOption(productOption)
+                    .quantity(quantity)
+                    .build();
+            cartItemRepository.save(cartItem);
+        }
+
     }
 }
