12771a6c3c3f8bd03b5cd62a02a637ef0d85555d
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-27 10:40:54 +0900

    예치금(cash) 개념 도입

---
+++ src/main/java/com/ll/exam/app_2022_09_22/app/cash/entity/CashLog.java
@@ -0,0 +1,25 @@
+package com.ll.exam.app_2022_09_22.app.cash.entity;
+
+import com.ll.exam.app_2022_09_22.app.base.entity.BaseEntity;
+import com.ll.exam.app_2022_09_22.app.member.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+// cashlog : 충전한 로그를 보여주는 곳
+public class CashLog extends BaseEntity {
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+    private long price; // 변동
+}
