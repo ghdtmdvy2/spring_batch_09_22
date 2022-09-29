dab499029746bbe63c3678d216fbfa68acf77c55
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-09-29 11:10:34 +0900

    복사본은 외래키를 제거하여 실제 데이터와 참조 되지 않도록

--- src/main/java/com/ll/exam/app_2022_09_22/app/order/entity/RebateOrderItem.java
+++ src/main/java/com/ll/exam/app_2022_09_22/app/order/entity/RebateOrderItem.java
@@ -20,17 +20,17 @@ import static javax.persistence.FetchType.LAZY;
 @ToString(callSuper = true)
 public class RebateOrderItem extends BaseEntity {
     @OneToOne(fetch = LAZY)
-
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     @ToString.Exclude
     private OrderItem orderItem;
 
     @ManyToOne(fetch = LAZY)
-
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     @ToString.Exclude
     private Order order;
 
     @ManyToOne(fetch = LAZY)
-
+    @JoinColumn(foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     private ProductOption productOption;
 
     private int quantity;
@@ -60,28 +60,4 @@ public class RebateOrderItem extends BaseEntity {
         isPaid = orderItem.isPaid();
     }
 
-    public RebateOrderItem(ProductOption productOption, int quantity) {
-        this.productOption = productOption;
-        this.quantity = quantity;
-        this.price = productOption.getPrice();
-        this.salePrice = productOption.getSalePrice();
-        this.wholesalePrice = productOption.getWholesalePrice();
-    }
-
-    public int calculatePayPrice() {
-        return salePrice * quantity;
-    }
-
-    public void setPaymentDone() {
-        this.pgFee = 0;
-        this.payPrice = calculatePayPrice();
-        this.isPaid = true;
-    }
-
-    public void setRefundDone() {
-        if (refundQuantity == quantity) return;
-
-        this.refundQuantity = quantity;
-        this.refundPrice = payPrice;
-    }
 }
